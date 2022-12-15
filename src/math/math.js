4328ce05c35051e6ea1a1c0c7e6fb417b4a891a7
Noah Wagner <wagnernoah@icloud.com>
2022-12-14 18:27:35 -0800

    added general handy math functions

---
+++ src/math/math.js
@@ -0,0 +1,28 @@
+/*
+This file is meant to hold any general handy math equations that will be used 
+in the core of the engine.
+*/
+
+/*
+remaps a number from the range s1-e1 to the new range s2-e2, expects 
+all inputs to be numbers
+*/
+export function remap(num, s1, e1, s2, e2) {
+	/*
+	works in steps:
+	1. move the first range start at origin
+	2. normalize the range by dividing by the length of the first range
+	3. multiply that by the length of the second range
+	4. add the offset of the second range
+	*/
+	return (num - s1) / (e1 - s1) * (e2 - s2) + s2;
+}
+
+/*
+constrains a number to be within the range min-max
+expects all integer inputs, returns x if it lies within the range,
+otherwise return min or max depending on if x is below or above the range
+*/
+export function constrain(num, min, max) {
+	return Math.min(Math.max(x, min), max);
+}
