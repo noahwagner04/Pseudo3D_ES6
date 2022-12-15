4429fe2560be8a349bc20adfcb82be5cd107441c
Noah Wagner <wagnernoah@icloud.com>
2022-12-14 20:30:19 -0800

    fixed import path

--- src/math/orientation.js
+++ src/math/orientation.js
@@ -3,7 +3,7 @@ this class represents the orientation of an object in 2d space
 it has two parts, a position vector and a direction vector
 */
 
-import { Vector } from "src/math/vector.js"
+import { Vector } from "/src/math/vector.js"
 
 export class Orientation {
 	// constructor expects 4 numbers as inputs
