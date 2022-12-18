86d547440943b3078713952cc384c237b32a1268
Noah Wagner <wagnernoah@icloud.com>
2022-12-17 18:55:33 -0800

    added basic functionality

---
+++ src/core/entity.js
@@ -0,0 +1,97 @@
+/*
+this class represents any physical object that will be in the raycast world
+any user made item / character class should use this class or derive from it
+*/
+
+import { Texture } from "/src/resources/texture.js";
+import { Color } from "/src/resources/color.js";
+import { Orientation } from "/src/math/orientation.js";
+
+export class Entity {
+	/*
+	config holds the appearance (texture or color), position and direction, 
+	and the size of the entity (in world coordinates)
+	*/
+	constructor(config) {
+		// check if we recieved a config object
+		if (typeof config !== "object") {
+			throw new Error(
+				"Entity constructor must recieve a config object"
+			);
+		}
+
+		// check if appearance argument is provided (neccessary)
+		if (!(config.appearance instanceof Texture) &&
+			!(config.appearance instanceof Color)) {
+			throw new Error(
+				"Entity constructor must receive an appearance argument of " +
+				"type Texture or Color"
+			);
+		}
+
+		let config_attributes = [
+			"position",
+			"direction",
+			"size"
+		];
+
+		// check if we recieved valid position, direction, and size attributes
+		for (let i = 0; i < config_attributes.length; i++) {
+			if (config[config_attributes[i]] !== undefined && (
+					typeof config[config_attributes[i]] !== "object" ||
+					typeof config[config_attributes[i]].x !== "number" ||
+					typeof config[config_attributes[i]].y !== "number")) {
+				throw new Error(
+					" Entity " +
+					config_attributes[i] +
+					" must be an object with x and y attributes that " +
+					"are numbers"
+				);
+			}
+		}
+
+		// check if the tint was passed properly
+		if (config.tint !== undefined && !(config.tint instanceof Color)) {
+			throw new Error("Entity tint must be of type Color");
+		}
+
+		// set appearance attribute
+		this.appearance = config.appearance;
+
+		// if position wasn't provided, use a the origin
+		let position = config.position || {
+			x: 0,
+			y: 0
+		};
+
+		// use a default direction if no direction vector was provided
+		let direction = config.direction || {
+			x: 1,
+			y: 0
+		};
+
+		// create an orientation object from the position and direction
+		this.orientation = new Orientation(
+			position.x,
+			position.y,
+			direction.x,
+			direction.y
+		);
+
+		// set the size to a default unit square if none was provided
+		this.size = config.size || {
+			x: 1,
+			y: 1
+		};
+
+		// a color to tint the appearance of the entity
+		this.tint = config.tint;
+
+		/*
+		set isInvisible and hasPartialAlpha to their corresponding 
+		truth / false values
+		*/
+		this.isInvisible = !!config.isInvisible;
+		this.hasPartialAlpha = !!config.hasPartialAlpha;
+	}
+}
