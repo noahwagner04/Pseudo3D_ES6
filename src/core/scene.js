7f05632b4697a6190e124ce27684177b27cd7004
Noah Wagner <wagnernoah@icloud.com>
2022-12-21 01:45:19 -0800

    finished all type checking

--- src/core/scene.js
+++ src/core/scene.js
@@ -1,3 +1,329 @@
+/*
+The scene class is meant to store world data, such as the world map, face 
+texture settings, objects in the scene, skybox texture, and lighting 
+information
+*/
+
+import Color from "/src/resources/color.js";
+import Texture from "/src/resources/texture.js";
+
+export default class Scene {
+	/*
+	config holds sub config objects which include: floor, ceiling, worldMap,
+	gameObject, skybox, lighting
+	NOTE: config object does get modified, scene attributes are referenced
+	to the config object
+	*/
+	constructor(config) {
+		// check if we recieved a config object
+		if (typeof config !== "object") {
+			throw new Error(
+				"Scene constructor must recieve a config object"
+			);
+		}
+
+		// a list of all the sub config objects
+		let subConfigs = [
+			"floor",
+			"ceiling",
+			"worldMap",
+			"gameObject",
+			"skybox",
+			"lighting"
+		];
+
+		// check if any of the sub config objects are valid
+		for (let i = 0; i < subConfigs.length; i++) {
+			let obj = config[subConfigs[i]];
+			if (obj !== undefined && typeof obj !== "object") {
+				throw new Error(
+					"Scene config." + subConfigs[i] + " must be an object"
+				);
+			}
+			// if the sub config wasn't provided, make it equal an empty object
+			config[subConfigs[i]] = obj || {};
+		}
+
+		// -----check all user input, sets all sub configs to be valid-----
+
+		checkWorldMap(config.worldMap);
+		checkFloorCeiling(config.floor, config.ceiling);
+		checkGameObject(config.gameObject);
+		checkSkybox(config.skybox);
+		checkLighting(config.lighting);
+
+		// assign this scenes attributes to sub configs which are now valid
+		this.worldMap = config.worldMap;
+		this.floor = config.floor;
+		this.ceiling = config.ceiling;
+		this.gameObject = config.gameObject;
+		this.skybox = config.skybox;
+		this.lighting = config.lighting;
+	}
+}
+
+// ----input handling functions, each modify the given object to be valid-----
+
+// checks worldMap object
+function checkWorldMap(worldMap) {
+	// check if worldMap.width is valid
+	if (worldMap.width !== undefined &&
+		(!Number.isInteger(worldMap.width) || worldMap.width <= 0)
+	) {
+		throw new Error("Scene worldMap.width must be a positive integer");
+	}
+
+	// check if worldMap.height is valid
+	if (worldMap.height !== undefined &&
+		(!Number.isInteger(worldMap.height) || worldMap.height <= 0)
+	) {
+		throw new Error("Scene worldMap.height must be a positive integer");
+	}
+
+	// if width or height wasn't provided, set them equal to 0
+	worldMap.width = worldMap.width || 0;
+	worldMap.height = worldMap.height || 0;
+
+	// check if worldMap.height is valid
+	if (worldMap.data !== undefined && !Array.isArray(worldMap.data)) {
+		throw new Error("Scene worldMap.data must be an array");
+	}
+
+	/*
+	if the data array wasn't passed, set it equal to an array with length
+	(worldMap.width * worldMap.height) filled with 0's
+	*/
+	if (worldMap.data === undefined) {
+		worldMap.data = [];
+		for (let i = 0; i < worldMap.height * worldMap.width; i++) {
+			worldMap.data[i] = 0;
+		}
+	}
+
+	// check if the data array passed has the correct length
+	if (worldMap.data.length !== worldMap.height * worldMap.width) {
+		throw new Error(
+			"Scene worldMap.data length must be worldMap.width *" +
+			" worldMap.height"
+		);
+	}
+
+	// check if the cellInfo object is valid
+	if (worldMap.cellInfo !== undefined &&
+		typeof worldMap.cellInfo !== "object"
+	) {
+		throw new Error("Scene worldMap.cellInfo must be an object");
+	}
+
+	// if cellInfo is not defined, set it to an empty object
+	worldMap.cellInfo = worldMap.cellInfo || {};
+
+	// check if each of the cellInfo attributes are correct
+	for (const property in worldMap.cellInfo) {
+		// first check if the attribute is an object
+		if (typeof worldMap.cellInfo[property] !== "object") {
+			throw new Error(
+				"Scene worldMap.cellInfo[\"" + property +
+				"\"] must be an object"
+			);
+		}
+
+		// the height of the wall
+		let height = worldMap.cellInfo[property].height;
+
+		// check if the height is a number
+		if (height !== undefined && typeof height !== "number") {
+			throw new Error(
+				"Scene worldMap.cellInfo[\"" + property +
+				"\"].height must be a number"
+			);
+		}
+
+		// if the height wasn't provided, default to a height of 1
+		worldMap.cellInfo[property].height = height === undefined ? 1 : height;
+
+		// the appearance of the wall, can be a texture or color
+		let appearance = worldMap.cellInfo[property].appearance;
+
+		// check if the appearance attribute is valid
+		if (appearance !== undefined &&
+			!(appearance instanceof Color) &&
+			!(appearance instanceof Texture)
+		) {
+			throw new Error(
+				"Scene worldMap.cellInfo[\"" + property +
+				"\"].appearance must be of type Color or Texture"
+			);
+		}
+
+		// if appearance wasn't provided, set it to a transparent color
+		worldMap.cellInfo[property].appearance = appearance ||
+			new Color(0, 0, 0, 0);
+	}
+}
+
+// checks floor or ceiling objects
+function checkFloorCeiling(floor, ceiling) {
+
+	/*
+	check if the appearance and cellWidth / cellHeight attributes are valid for
+	both the ceiling and the floor
+	*/
+	for (let i = 0; i < 2; i++) {
+		let plane = i === 0 ? floor : ceiling;
+		let debugPlane = i === 0 ? "floor" : "ceiling";
+
+		// check if the appearance attribute for the floor / ceiling is valid
+		if (plane.appearance !== undefined &&
+			!(plane.appearance instanceof Color) &&
+			!(plane.appearance instanceof Texture)
+		) {
+			throw new Error(
+				"Scene " + debugPlane +
+				".appearance must be of type Color or Texture"
+			);
+		}
+
+		/*
+		if no floor / ceiling appearance was provided, set it to a transparent 
+		color
+		*/
+		plane.appearance = plane.appearance || new Color(0, 0, 0, 0);
+
+		// check if floor / ceiling cellWidth is valid
+		if (plane.cellWidth !== undefined &&
+			typeof plane.cellWidth !== "number"
+		) {
+			throw new Error(
+				"Scene " + debugPlane + ".cellWidth must be a number"
+			);
+		}
+
+		// check if floor / ceiling cellHeight is valid
+		if (plane.cellHeight !== undefined &&
+			typeof plane.cellHeight !== "number"
+		) {
+			throw new Error(
+				"Scene " + debugPlane + ".cellHeight must be a number"
+			);
+		}
+
+		// if neither cellWidth or cellHeight was provided, default to 1
+		plane.cellWidth = plane.cellWidth || 1;
+		plane.cellHeight = plane.cellHeight || 1;
+	}
+
+	// check if the ceiling height attribute is valid
+	if (ceiling.height !== undefined && typeof ceiling.height !== "number") {
+		throw new Error("Scene ceiling.height must be a number");
+	}
+
+	// if no height was provided, default to 1
+	ceiling.height = ceiling.height || 1;
+}
+
+// checks gameObject object
+function checkGameObject(gameObject) {
+	// check if the passed entity array is valid
+	if (gameObject.entities !== undefined &&
+		!Array.isArray(gameObject.entities)
+	) {
+		throw new Error(
+			"Scene gameObject.entities must be an array of Entities"
+		);
+	}
+
+	// if no array was provided, set it to an empty array
+	gameObject.entities = gameObject.entities || [];
+
+	// check if resolutionX is valid
+	if (gameObject.resolutionX !== undefined &&
+		(!Number.isInteger(gameObject.resolutionX) ||
+			gameObject.resolutionX < 0)
+	) {
+		throw new Error(
+			"Scene gameObject.resolutionX must be a positive integer"
+		);
+	}
+
+	// check if resolutionY is valid
+	if (gameObject.resolutionY !== undefined &&
+		(!Number.isInteger(gameObject.resolutionY) ||
+			gameObject.resolutionY < 0)
+	) {
+		throw new Error(
+			"Scene gameObject.resolutionY must be a positive integer"
+		);
+	}
+
+	// if resolutionX / resolutionY wasn't provided, default to 64 * 64
+	gameObject.resolutionX = gameObject.resolutionX || 64;
+	gameObject.resolutionY = gameObject.resolutionY || 64;
+}
+
+// checks skybox object
+function checkSkybox(skybox) {
+	// check if the provided appearance attribute is valid
+	if (skybox.appearance !== undefined &&
+		!(skybox.appearance instanceof Color) &&
+		!(skybox.appearance instanceof Texture)
+	) {
+		throw new Error(
+			"Scene skybox.appearance must be of type Color or Texture"
+		);
+	}
+
+	// if no appearance was provided, use a transparent color
+	skybox.appearance = skybox.appearance || new Color(0, 0, 0, 0);
+
+	// check if repeatAfterAngle is valid
+	if (skybox.repeatAngle !== undefined &&
+		typeof skybox.repeatAngle !== "number"
+	) {
+		throw new Error("Scene skybox.repeatAngle must be a number");
+	}
+
+	/*
+	if no angle was provided, set it to 2PI (the amount of radians untill the
+	skybox starts to repeat)
+	*/
+	skybox.repeatAngle = skybox.repeatAngle || 360;
+
+	// set a flag if we should render the skybox or not
+	skybox.enabled = !!(skybox.appearance.alpha);
+}
+
+// checks lighting object
+function checkLighting(lighting) {
+	// check if provided side light is valid
+	if (lighting.sideLight !== undefined &&
+		typeof lighting.sideLight !== "number"
+	) {
+		throw new Error("Scene lighting.sideLight must be a number");
+	}
+
+	// default to 1 if no sideLight was provided
+	lighting.sideLight = lighting.sideLight || 1;
+
+	// check if ambientLight is valid
+	if (lighting.ambientLight !== undefined &&
+		typeof lighting.ambientLight !== "number"
+	) {
+		throw new Error("Scene lighting.ambientLight must be a number");
+	}
+
+	// default to 1 if no ambientLight was provided
+	lighting.ambientLight = lighting.ambientLight === undefined ? 1 :
+		lighting.ambientLight;
+
+	/*
+	set a flag indecating whether or not lighting is enabled (if both ambient
+	and side light are 1 then disable lighting)
+	*/
+	lighting.enabled = !((lighting.sideLight === 1) &&
+		(lighting.ambientLight === 1));
+}
+
 // worldWidth
 // worldHeight
 // worldMap
